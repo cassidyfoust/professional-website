32395cbb4aa6a21eb50f02a93c0077733ab7dc05
Cassidy Foust <cassidy.foust@gmail.com>
2019-11-03 17:23:24 -0600

    edited copy

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -18,14 +18,14 @@ class Home extends Component {
                   <img src={Text} className="text-img"></img>
                 </div>
                 <div className="home-text">
-                  I'm a former publishing professional turned full stack
-                  software developer. With a background in marketing and graphic
+                  I'm a former publishing professional turned front end web developer. =
+                  With a background in marketing and graphic
                   design, my passion is merging elegant code with elegant
-                  design. I believe even the smartest code is useless if users
-                  don't want to interact with it. I graduated from Macalester
+                  design. I graduated from Macalester
                   College in 2015 with a B.A. in Creative Writing and Prime
                   Digital Academy in 2019 with a certificate in Full Stack
-                  Software Engineering.
+                  Software Engineering. I'm available for freelance web development
+                  and design.
                 </div>
                 <div className="info-wrapper">
                   <img src={SkillsImg} className="header-img"></img>
